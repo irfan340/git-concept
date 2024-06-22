5227b46a36c011d467e1d7e90eedf4e1dc963e7b
irfan340 <61534088+irfan340@users.noreply.github.com.>
2024-06-22 21:55:26 +0600

    learn commit

---
+++ git&github.js
@@ -0,0 +1,9 @@
+/*
+1.what is git and github?
+2. two types of directory -i) local ii) remote
+3.flow of local directory-
+    working directory-stage-local repository
+
+4. git status(what has been changed)
+5. difference between (add .) and (add *) 
+*/  
---
+++ setup.js
@@ -0,0 +1,6 @@
+/*
+1.git init(working directory)
+2.git add(stage)
+or, 3. git reset
+4. 
+*/ 
